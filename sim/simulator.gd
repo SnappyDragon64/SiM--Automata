14fb08273aafb69f4b80564a9eefdad54a018425
SnappyDragon64 <pg61151@gmail.com>
2023-09-15 15:48:54 +0530

    e38392e382abe383aae382ace3839fe382a8e383ab

--- sim/simulator.gd
+++ sim/simulator.gd
@@ -2,12 +2,29 @@ extends Control
 
 @onready var interface = $interface_layer/interface
 
+var default_theme = preload('res://asset/theme/default.tres')
+var light_theme = preload('res://asset/theme/light.tres')
+var debug_theme = preload('res://asset/theme/debug.tres')
+
+var debug_sequence = [4194320, 4194320, 4194322, 4194322, 4194319, 4194321, 4194319, 4194321, 66, 65]
+var current_sequence = []
+
 func _ready():
 	DisplayServer.window_set_min_size(Vector2(640, 480))
 	Signals.dark_mode.connect(_dark_mode)
 
 func _dark_mode(flag):
 	if flag:
-		interface.set_theme(load('res://asset/theme/default.tres'))
+		interface.set_theme(default_theme)
 	else:
-		interface.set_theme(load('res://asset/theme/light.tres'))
+		interface.set_theme(light_theme)
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		current_sequence.append(event.get_key_label())
+		
+		if len(current_sequence) > 10:
+			current_sequence.pop_front()
+		
+		if current_sequence == debug_sequence:
+			interface.set_theme(debug_theme)
